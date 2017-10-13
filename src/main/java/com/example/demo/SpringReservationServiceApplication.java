c3a084df9fade413ccbf921a85bdeafbb6d3feca
jackyhuangc <huangchao911@aliyun.com>
2017-10-13 18:19:53 +0800

    xxx

--- src/main/java/com/example/demo/SpringReservationServiceApplication.java
+++ src/main/java/com/example/demo/SpringReservationServiceApplication.java
@@ -1,5 +1,12 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.io.Reader;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -20,6 +27,34 @@ public class SpringReservationServiceApplication {
 	@RequestMapping("/hello2")
 	String index2(String str) {
 		return "Hello Spring Boot"+str;
+	}
+	
+	@GET
+	@RequestMapping("/getorderbill")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayHelloWorld() {
+//
+//		try {
+//			// 1、获取资源文件
+//			Reader reader = Resources.getResourceAsReader("mybatisconfig.xml");
+//
+//			// 2、得到SessionFactory,使用类加载获取xml文件
+//			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(reader);
+//
+//			// 3、打开session，创建能执行配置文件中的sql:sqlsession
+//			SqlSession session = factory.openSession();
+//
+//			// 4、查询一个对象：session.selectOne(注册过的命名空间sql映射+select的ID)
+//			PRODUCT p = session.selectOne("A.B.C.selectPRODUCTById", 25);
+//
+//			System.out.println(p.getPRODUCTNAME());
+//			return p.getPRODUCTCOMPANY();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return "Hello World!";
 	}
 	
 	public static void main(String[] args) {
